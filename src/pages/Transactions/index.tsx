5cd6f235ba8b595290bde604f9a5e848b70429f0
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2023-02-21 17:03:50 -0300

    Added table layout
    Added table layout

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,7 +1,12 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm/index";
-import { TransactionsContainer } from "./styles";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+import transactions from "../../../server.json";
 
 export function Transactions() {
   return (
@@ -11,6 +16,26 @@ export function Transactions() {
 
       <TransactionsContainer>
         <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            {transactions.transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === "outcome" && "- "}
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </TransactionsTable>
       </TransactionsContainer>
     </div>
   );
